ab1c2ec47a635e9fc224a14cc15f9c3808d20c57
Arpan Banerjee <arpanbnrj9@gmail.com>
2017-03-24 11:25:57 +0530

    Added command to check genome of held seed

--- src/main/java/org/terasology/simpleFarming/systems/SeedMixerSystem.java
+++ src/main/java/org/terasology/simpleFarming/systems/SeedMixerSystem.java
@@ -19,10 +19,15 @@ import org.terasology.entitySystem.entity.EntityRef;
 import org.terasology.entitySystem.event.ReceiveEvent;
 import org.terasology.entitySystem.systems.BaseComponentSystem;
 import org.terasology.entitySystem.systems.RegisterSystem;
+import org.terasology.genome.component.GenomeComponent;
 import org.terasology.genome.events.OnBreed;
+import org.terasology.logic.characters.CharacterHeldItemComponent;
+import org.terasology.logic.console.commandSystem.annotations.Command;
+import org.terasology.logic.console.commandSystem.annotations.Sender;
 import org.terasology.logic.inventory.InventoryComponent;
 import org.terasology.logic.inventory.InventoryManager;
 import org.terasology.logic.inventory.events.BeforeItemPutInInventory;
+import org.terasology.network.ClientComponent;
 import org.terasology.registry.In;
 import org.terasology.simpleFarming.components.SeedComponent;
 import org.terasology.simpleFarming.components.SeedMixerComponent;
@@ -74,6 +79,21 @@ public class SeedMixerSystem extends BaseComponentSystem {
                 event.getOrganism1().destroy();
                 event.getOrganism2().destroy();
             }
+        }
+    }
+
+    @Command(shortDescription = "Prints genome of held seed.")
+    public String seedGenomeCheck(@Sender EntityRef client) {
+        EntityRef character = client.getComponent(ClientComponent.class).character;
+        if (character.hasComponent(CharacterHeldItemComponent.class)) {
+            EntityRef selectedItem = character.getComponent(CharacterHeldItemComponent.class).selectedItem;
+            if (selectedItem.hasComponent(SeedComponent.class) && selectedItem.hasComponent(GenomeComponent.class)) {
+                return selectedItem.getComponent(GenomeComponent.class).genes;
+            } else {
+                return "Command not valid for current conditions.";
+            }
+        } else {
+            return "Command not valid for current conditions.";
         }
     }
 }
