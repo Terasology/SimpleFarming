49453f79c9e6d36306852058574e3bc15290a764
Arpan Banerjee <arpanbnrj9@gmail.com>
2017-04-05 17:59:08 +0530

    Made new window class

--- src/main/java/org/terasology/simpleFarming/ui/SeedMixerScreen.java
+++ src/main/java/org/terasology/simpleFarming/ui/SeedMixerScreen.java
@@ -15,11 +15,11 @@
  */
 package org.terasology.simpleFarming.ui;
 
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.terasology.entitySystem.entity.EntityRef;
 import org.terasology.logic.characters.CharacterComponent;
-import org.terasology.logic.inventory.InventoryComponent;
 import org.terasology.logic.players.LocalPlayer;
 import org.terasology.registry.CoreRegistry;
 import org.terasology.rendering.nui.BaseInteractionScreen;
@@ -28,65 +28,20 @@ import org.terasology.rendering.nui.databinding.ReadOnlyBinding;
 import org.terasology.rendering.nui.layers.ingame.inventory.InventoryCell;
 import org.terasology.rendering.nui.layers.ingame.inventory.InventoryGrid;
 
-/**
- * This class represents the seed mixer window.
- */
-public class SeedMixerWindow extends BaseInteractionScreen {
+public class SeedMixerScreen extends BaseInteractionScreen {
+
+    private EntityRef player = EntityRef.NULL;
 
     private InventoryGrid playerInventory;
-
-    private static final Logger logger = LoggerFactory.getLogger(SeedMixerWindow.class);
+    private static final Logger logger = LoggerFactory.getLogger(SeedMixerScreen.class);
 
     private InventoryCell seed1;
     private InventoryCell seed2;
     private InventoryCell offspring1;
     private InventoryCell offspring2;
 
-    private EntityRef player = EntityRef.NULL;
-
-    @Override
-    public void initialise() {
-        logger.info("Initialise called.");
-        player = EntityRef.NULL;
-
-        seed1 = find("S1", InventoryCell.class);
-        seed2 = find("S1", InventoryCell.class);
-        offspring1 = find("O1", InventoryCell.class);
-        offspring2 = find("O2", InventoryCell.class);
-
-        playerInventory = find("inventory", InventoryGrid.class);
-        player = CoreRegistry.get(LocalPlayer.class).getCharacterEntity();
-        playerInventory.bindTargetEntity(new ReadOnlyBinding<EntityRef>() {
-            @Override
-            public EntityRef get() {
-                return player;
-            }
-        });
-        playerInventory.setCellOffset(10);
-        playerInventory.setMaxCellCount(40);
-    }
-
-    @Override
-    public void onOpened() {
-        logger.info("onOpened");
-        EntityRef characterEntity = CoreRegistry.get(LocalPlayer.class).getCharacterEntity();
-        CharacterComponent characterComponent = characterEntity.getComponent(CharacterComponent.class);
-
-        // In case the player has not been created yet, exit out early to prevent an error.
-        if (characterComponent == null) {
-            return;
-        }
-
-        // As long as there's an interaction target, open this window.
-        if (getInteractionTarget() != EntityRef.NULL) {
-            initializeWithInteractionTarget(getInteractionTarget());
-            super.onOpened();
-        }
-    }
-
     @Override
     protected void initializeWithInteractionTarget(EntityRef interactionTarget) {
-        logger.info(interactionTarget.toFullDescription());
         seed1.bindTargetInventory(new Binding<EntityRef>() {
             @Override
             public EntityRef get() {
@@ -117,7 +72,7 @@ public class SeedMixerWindow extends BaseInteractionScreen {
 
             @Override
             public void set(EntityRef value) {
-                seed1.setTargetInventory(value);
+                offspring1.setTargetInventory(value);
             }
         });
         offspring2.bindTargetInventory(new Binding<EntityRef>() {
@@ -128,9 +83,55 @@ public class SeedMixerWindow extends BaseInteractionScreen {
 
             @Override
             public void set(EntityRef value) {
-                seed1.setTargetInventory(value);
+                offspring2.setTargetInventory(value);
             }
         });
     }
 
+    private void reInit() {
+        player = CoreRegistry.get(LocalPlayer.class).getCharacterEntity();
+        playerInventory = find("inventory", InventoryGrid.class);
+        playerInventory.bindTargetEntity(new ReadOnlyBinding<EntityRef>() {
+            @Override
+            public EntityRef get() {
+                return player;
+            }
+        });
+        playerInventory.setCellOffset(10);
+    }
+
+    @Override
+    public void initialise() {
+        player = EntityRef.NULL;
+
+        seed1 = find("S1", InventoryCell.class);
+        seed2 = find("S1", InventoryCell.class);
+        offspring1 = find("O1", InventoryCell.class);
+        offspring2 = find("O2", InventoryCell.class);
+    }
+
+    @Override
+    public void onOpened() {
+        EntityRef characterEntity = CoreRegistry.get(LocalPlayer.class).getCharacterEntity();
+        CharacterComponent characterComponent = characterEntity.getComponent(CharacterComponent.class);
+
+        // In case the player has been created yet, exit out early to prevent an error.
+        if (characterComponent == null) {
+            logger.info("character component null");
+            return;
+        }
+
+        // If the reference to the player entity hasn't been set yet, or it refers to a NULL entity, call the reInit()
+        // method to set it. The getId() check is necessary for certain network entities whose ID is 0, but are
+        // erroneously marked as existent.
+        if (!player.exists() || (player.exists() && (player == EntityRef.NULL || player.getId() == 0 || player == null))) {
+            reInit();
+        }
+
+        // As long as there's an interaction target, open this window.
+        if (getInteractionTarget() != EntityRef.NULL) {
+            initializeWithInteractionTarget(getInteractionTarget());
+            super.onOpened();
+        }
+    }
 }
