c003823557533b3a5fc92559e7f9ec95ac18cfda
Aaron Harris <aaron.patrick.harris@gmail.com>
2017-11-14 11:32:25 -0600

    Correct indentation (was 2 spaces, should have been 4)

--- src/test/java/org/terasology/simpleFarming/systems/PlantAuthoritySystemTest.java
+++ src/test/java/org/terasology/simpleFarming/systems/PlantAuthoritySystemTest.java
@@ -39,127 +39,126 @@ import java.util.Set;
 
 public class PlantAuthoritySystemTest extends ModuleTestingEnvironment {
 
-  /** The block our test bush should use. */
-  private static final String BUSH_BLOCK = "SimpleFarming:BaseBush";
+    /** The block our test bush should use. */
+    private static final String BUSH_BLOCK = "SimpleFarming:BaseBush";
 
-  private EntityManager entityManager;
-  private WorldProvider worldProvider;
+    private EntityManager entityManager;
+    private WorldProvider worldProvider;
 
-  private Block air;
-  private Block dirt;
-  private Block bush;
+    private Block air;
+    private Block dirt;
+    private Block bush;
 
-  @Before
-  public void initialize() {
-    entityManager = getHostContext().get(EntityManager.class);
-    worldProvider = getHostContext().get(WorldProvider.class);
+    @Before
+    public void initialize() {
+        entityManager = getHostContext().get(EntityManager.class);
+        worldProvider = getHostContext().get(WorldProvider.class);
 
-    BlockManager blockManager = getHostContext().get(BlockManager.class);
-    air = blockManager.getBlock("engine:air");
-    dirt = blockManager.getBlock("core:dirt");
-    bush = blockManager.getBlock(BUSH_BLOCK);
-  }
-
-  @Override
-  public Set<String> getDependencies() {
-    return Collections.singleton("SimpleFarming");
-  }
-
-  @Test
-  public void seedShouldGrowWherePlanted() {
-    plantSeedTest(dirt, air, true);
-  }
-
-  @Test
-  public void seedShouldNotGrowOnAir() {
-    plantSeedTest(air, air, false);
-  }
-
-  @Test
-  public void seedShouldNotReplaceBlock() {
-    plantSeedTest(dirt, dirt, false);
-  }
-
-  /**
-   * Generic test trying to plant a seed and observing the result.
-   * <p>
-   * Sets the target block and the block above the target to the given materials and
-   * attempts to plant a seed on the target block.  Then makes assertions according
-   * to the value of {@code shouldSucceed}: if {@code shouldSucceed} is true, the
-   * block above the target should now be the block specified by {@code BUSH_BLOCK};
-   * if not, it should remain {@code materialAbove}.  In either case, the target
-   * block should be unchanged.
-   *
-   * @param material the block the seed should be planted on
-   * @param materialAbove the block above the target block
-   * @param shouldSucceed whether the planting is expected to succeed
-   */
-  private void plantSeedTest(Block material, Block materialAbove, boolean shouldSucceed) {
-    final Vector3i targetPosition = Vector3i.zero();
-    final Vector3i aboveTargetPosition = new Vector3i(targetPosition).add(Vector3i.up());
-
-    setBlock(targetPosition, material);
-    setBlock(aboveTargetPosition, materialAbove);
-    plantSeed(makeTestSeed(), targetPosition);
-
-    final Block expected = shouldSucceed ? bush : materialAbove;
-    assertEquals(expected, worldProvider.getBlock(aboveTargetPosition));
-    assertEquals(material, worldProvider.getBlock(targetPosition));
-  }
-
-  /**
-   * Fills the given region with the specified block.
-   * <p>
-   * Also ensures that all blocks in and adjacent to the region are loaded.
-   */
-  private void fillRegion(Region3i region, Block material) {
-    Region3i loadRegion = region.expand(1);
-    for (Vector3i pos : loadRegion) {
-      forceAndWaitForGeneration(pos);
+        BlockManager blockManager = getHostContext().get(BlockManager.class);
+        air = blockManager.getBlock("engine:air");
+        dirt = blockManager.getBlock("core:dirt");
+        bush = blockManager.getBlock(BUSH_BLOCK);
     }
 
-    for (Vector3i pos : region) {
-      worldProvider.setBlock(pos, material);
+    @Override
+    public Set<String> getDependencies() {
+        return Collections.singleton("SimpleFarming");
     }
-  }
 
-  /**
-   * Sets the block at the given position.
-   * <p>
-   * Also ensures that all blocks adjacent to the position are loaded.
-   */
-  private void setBlock(Vector3i position, Block material) {
-    fillRegion(Region3i.createFromCenterExtents(position, 0), material);
-  }
+    @Test
+    public void seedShouldGrowWherePlanted() {
+        plantSeedTest(dirt, air, true);
+    }
 
-  /**
-   * Creates a seed entity, using parameters appropriate to the test.
-   * <p>
-   * To avoid coupling the test to any particular seed prefab, generates a seed definition from
-   * scratch, with a null prefab and a basic, single-stage bush definition on the same entity.
-   */
-  private EntityRef makeTestSeed() {
-    final BushDefinitionComponent bushDefinition = new BushDefinitionComponent();
-    bushDefinition.growthStages = Collections.singletonMap(BUSH_BLOCK, new GrowthStage());
+    @Test
+    public void seedShouldNotGrowOnAir() {
+        plantSeedTest(air, air, false);
+    }
 
-    return entityManager.create(
-        new SeedDefinitionComponent(),
-        bushDefinition
-    );
-  }
+    @Test
+    public void seedShouldNotReplaceBlock() {
+        plantSeedTest(dirt, dirt, false);
+    }
 
-  /** Attempts to plant the given seed at the given position. */
-  private void plantSeed(EntityRef seed, Vector3i position) {
-    final EntityRef target = entityManager.create(new LocationComponent(position.toVector3f()));
-    final ActivateEvent event = new ActivateEvent(
-        target,                 // target
-        EntityRef.NULL,         // instigator
-        null,                   // origin
-        null,                   // direction
-        position.toVector3f(),  // hit position
-        Vector3f.up(),          // hit normal
-        0                       // activation id
-    );
-    seed.send(event);
-  }
+    /**
+     * Generic test trying to plant a seed and observing the result.
+     * <p>
+     * Sets the target block and the block above the target to the given materials and attempts to
+     * plant a seed on the target block.  Then makes assertions according to the value of
+     * {@code shouldSucceed}: if {@code shouldSucceed} is true, the block above the target should
+     * now be the block specified by {@code BUSH_BLOCK}; if not, it should remain
+     * {@code materialAbove}.  In either case, the target block should be unchanged.
+     *
+     * @param material the block the seed should be planted on
+     * @param materialAbove the block above the target block
+     * @param shouldSucceed whether the planting is expected to succeed
+     */
+    private void plantSeedTest(Block material, Block materialAbove, boolean shouldSucceed) {
+        final Vector3i targetPosition = Vector3i.zero();
+        final Vector3i aboveTargetPosition = new Vector3i(targetPosition).add(Vector3i.up());
+
+        setBlock(targetPosition, material);
+        setBlock(aboveTargetPosition, materialAbove);
+        plantSeed(makeTestSeed(), targetPosition);
+
+        final Block expected = shouldSucceed ? bush : materialAbove;
+        assertEquals(expected, worldProvider.getBlock(aboveTargetPosition));
+        assertEquals(material, worldProvider.getBlock(targetPosition));
+    }
+
+    /**
+     * Fills the given region with the specified block.
+     * <p>
+     * Also ensures that all blocks in and adjacent to the region are loaded.
+     */
+    private void fillRegion(Region3i region, Block material) {
+        Region3i loadRegion = region.expand(1);
+        for (Vector3i pos : loadRegion) {
+            forceAndWaitForGeneration(pos);
+        }
+
+        for (Vector3i pos : region) {
+            worldProvider.setBlock(pos, material);
+        }
+    }
+
+    /**
+     * Sets the block at the given position.
+     * <p>
+     * Also ensures that all blocks adjacent to the position are loaded.
+     */
+    private void setBlock(Vector3i position, Block material) {
+        fillRegion(Region3i.createFromCenterExtents(position, 0), material);
+    }
+
+    /**
+     * Creates a seed entity, using parameters appropriate to the test.
+     * <p>
+     * To avoid coupling the test to any particular seed prefab, generates a seed definition from
+     * scratch, with a null prefab and a basic, single-stage bush definition on the same entity.
+     */
+    private EntityRef makeTestSeed() {
+        final BushDefinitionComponent bushDefinition = new BushDefinitionComponent();
+        bushDefinition.growthStages = Collections.singletonMap(BUSH_BLOCK, new GrowthStage());
+
+        return entityManager.create(
+                new SeedDefinitionComponent(),
+                bushDefinition
+        );
+    }
+
+    /** Attempts to plant the given seed at the given position. */
+    private void plantSeed(EntityRef seed, Vector3i position) {
+        final EntityRef target = entityManager.create(new LocationComponent(position.toVector3f()));
+        final ActivateEvent event = new ActivateEvent(
+                target,                 // target
+                EntityRef.NULL,         // instigator
+                null,                   // origin
+                null,                   // direction
+                position.toVector3f(),  // hit position
+                Vector3f.up(),          // hit normal
+                0                       // activation id
+        );
+        seed.send(event);
+    }
 }
