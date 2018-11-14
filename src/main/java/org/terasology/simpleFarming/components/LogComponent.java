4449b8c336cd41cba1100556480716be2adbcfea
ContronPAnda <rajtel99@gmail.com>
2018-11-14 04:17:25 -0500

    Added Javadocs to LogComponent and RootComponent

--- src/main/java/org/terasology/simpleFarming/components/LogComponent.java
+++ src/main/java/org/terasology/simpleFarming/components/LogComponent.java
@@ -3,12 +3,27 @@ package org.terasology.simpleFarming.components;
 import org.terasology.entitySystem.Component;
 import org.terasology.entitySystem.entity.EntityRef;
 import org.terasology.math.geom.Vector3i;
+import org.terasology.simpleFarming.systems.TreeAuthoritySystem;
 import org.terasology.world.block.ForceBlockActive;
 
+/**
+ * Represents any log in a tree, including the root.
+ * Created automatically by {@link TreeAuthoritySystem}.
+ * 
+ * @see RootComponent
+ */
 @ForceBlockActive
 public class LogComponent implements Component {
 	
+	/**
+	 * The location of this log in the world.
+	 */
 	public Vector3i location;
 	
+	/**
+	 * The root associated with this log's tree. This reference helps
+	 * to identify which tree this log is part of and send important
+	 * events to the root so that the whole tree can react.
+	 */
 	public EntityRef root;
 }
--- src/main/java/org/terasology/simpleFarming/components/RootComponent.java
+++ src/main/java/org/terasology/simpleFarming/components/RootComponent.java
@@ -8,26 +8,64 @@ import java.util.Set;
 import org.terasology.entitySystem.Component;
 import org.terasology.entitySystem.entity.EntityRef;
 import org.terasology.entitySystem.prefab.Prefab;
+import org.terasology.simpleFarming.systems.TreeAuthoritySystem;
 import org.terasology.world.block.Block;
 import org.terasology.world.block.ForceBlockActive;
 
+/**
+ * Represents the root (lowest log block) of a tree.
+ * <p>
+ * This component is automatically created by {@link TreeAuthoritySystem}. It
+ * holds all of the information that relates to the tree in general as opposed
+ * to a single block in specific.
+ */
 @ForceBlockActive
 public class RootComponent implements Component {
 	
+	/**
+	 * The block to use for the sapling in the world.
+	 */
 	public Block sapling;
 	
+	/**
+	 * The block to use for a log in the tree in the world.
+	 */
 	public Block log;
 	
+	/**
+	 * The prefab for the bush to use for the leaf block. This prefab should have
+	 * a {@link BushDefinitionComponent}
+	 */
 	public Prefab leaf;
 	
+	/**
+	 * The stages which this tree should grow through, in order.
+	 */
 	public List<TreeGrowthStage> growthStages = new ArrayList<>();
 	
+	/**
+	 * The current growth stage, represents the index in {@link #growthStages}.
+	 */
 	public int growthStage = 0;
 	
+	/**
+	 * Holds references to all of the leaves in this tree. Note that leaves aren't
+	 * automatically removed from this set when they no longer exist, so any element
+	 * in this set should be verified with {@link EntityRef#exists()}.
+	 */
 	public Set<EntityRef> leaves = new HashSet<>();
 	
+	/**
+	 * Whether or not the tree is alive. The tree is no longer alive if any of the
+	 * log blocks are destroyed, however leaves are not considered. The tree will
+	 * only be able to grow/ungrow (with a {@link CheatGrowthComponent}) if it is
+	 * alive.
+	 */
 	public boolean alive = true;
 	
+	/**
+	 * Default constructor required for persistence.
+	 */
 	public RootComponent() {
 		
 	}
